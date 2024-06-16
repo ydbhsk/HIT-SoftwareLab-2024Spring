272e0ae48b7a433210238726486d38d4ac469e6f
ydbhsk <62494411+ydbhsk@users.noreply.github.com>
2024-06-16 23:56:19 +0800

    update file

--- src/main/java/MyTest.java
+++
@@ -1,9 +0,0 @@
-
-public class MyTest {
-    public static void main(String[] args) throws InterruptedException {
-        for (int i = 1; i <= 100; i++) {
-            System.out.print("\r" + i);
-            Thread.sleep(100);  // 暂停100毫秒以模拟实时更新
-        }
-    }
-}
